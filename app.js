439a9394dea0fd7aca1ca8798f3eca85ea0acea8
Igor Pronin <legalsolut@ya.ru>
2020-02-05 12:31:53 +0700

    Send message func

--- app.js
+++ app.js
@@ -13,8 +13,39 @@ app.post('/', (req, res, next) => {
   if (process.env.DEBUG_MODE) {
     console.log(req.body);
   }
+  const chatId = req.message.chat.id;
+  const userId = req.message.from.id;
+  const username = req.message.from.username;
+  const text = req.message.text;
+  let mes;
+  if (chatId === userId) {
+    switch (text) {
+      case '/h':
+        mes = `<b>Список команд:</b>
+/h - помощь`;
+        sendMes(mes);
+        break;
+      default:
+        console.log('[Error] Unknown command.')
+    }
+  }
   res.end();
 });
+
+function sendMes(mes, userId) {
+  axios.post(`https://api.telegram.org/bot${process.env.BOT_ID}:${process.env.BOT_TOKEN}/sendMessage`, {
+    chat_id: userId,
+    text: mes,
+    parse_mode: 'HTML'
+  })
+  .then(response => {
+    console.log(`[Message sent] Message sent to user with id ${userId}`);
+    // console.log(response.data.explanation);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+}
 
 app.listen(process.env.PORT, () => {
   console.log(`Telegram server is listening on port ${process.env.PORT}...`);
